e1749485d152a0adb9d42c59b8f8fa23e518ebbe
Jackbringas <102544709+Jackbringas@users.noreply.github.com>
2025-04-18 17:38:09 +0000

    clear all funcionando correctamente

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,122 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
+  const username = "jackbringas"; 
+  const apiUrl = "https://playground.4geeks.com/todo";
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const createUser = async () => {
+    try {
+      const response = await fetch(apiUrl + "/users/" + username, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+      });
+      if (response.status === 201) {
+        getToDos();  
+      } else {
+        const data = await response.json();
+        alert("Error: " + data.detail);
+      }
+    } catch (error) {
+      console.error("❌ Error verifying or creating user:", error);
+    }
+  };
+
+  const getToDos = async () => {
+    const response = await fetch(apiUrl + "/users/" + username);
+    if (response.status === 404) {
+      createUser();  
+    } else if (response.status === 200) {
+      const data = await response.json();
+      setTodos(data.todos);
+    } else {
+      alert("We are not able to retrieve your tasks. Please try again later.");
+    }
+  };
+
+  useEffect(() => {
+    getToDos();
+  }, []);
+
+  const addTodo = async (e) => {
+    if (e.key === "Enter" && inputValue.trim() !== "") {
+      const newTodo = { label: inputValue };
+      const response = await fetch(apiUrl + "/todos/" + username, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(newTodo),
+      });
+      if (response.status === 201) {
+        setInputValue("");  
+        getToDos(); 
+      } else {
+        const data = await response.json();
+        alert("Error: " + data.detail);
+      }
+    }
+  };
+
+  const deleteTodo = async (id) => {
+    const response = await fetch(apiUrl + "/todos/" + id, {
+      method: "DELETE",
+    });
+    if (response.status === 204) {
+      getToDos();  
+    } else {
+      const data = await response.json();
+      alert("Error: " + data.detail);
+    }
+  };
+
+  const clearTodos = async () => {
+    const response = await fetch(apiUrl + "/users/" + username, {
+      method: "DELETE",
+    });
+    if (response.status === 204) {
+     createUser()  
+    } else {
+      const data = await response.json();
+      alert("Error: " + data.detail);
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1>TODOS</h1>
+      <ul className="todo-list">
+        <li>
+          <input
+            type="text"
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+            onKeyDown={addTodo}
+            placeholder="What needs to be done?"
+          />
+        </li>
+
+        {todos.length === 0 ? (
+          <p className="empty-message">No tasks, add one!</p>
+        ) : (
+          todos.map((task) => (
+            <li key={task.id} className="todo-item">
+              {task.label}
+              <button
+                type="button"
+                className="btn-close"
+                aria-label="Close"
+                onClick={() => deleteTodo(task.id)}
+              ></button>
+            </li>
+          ))
+        )}
+      </ul>
+      <div>{todos.length} items left</div>
+      <button className="btn btn-danger mt-3" onClick={clearTodos}>
+        Clear All
+      </button>
+    </div>
+  );
 };
 
 export default Home;
